6924357be1c1eacdcabe86c7aa850ea67d6c7757
Mohand Arezki <braniciarezki@gmail.com>
2024-12-06 06:12:37 +0100

    grid impl

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import {AppCaptchaComponent} from "./Components/app-captcha/app-captcha.component";
 
 const routes: Routes = [
+  { path: '', redirectTo: 'app-captcha', pathMatch: 'full' },
   { path: 'app-captcha', component: AppCaptchaComponent }
 ];
 
